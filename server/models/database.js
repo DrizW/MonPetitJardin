8c9a7bb44c073cfaf3f22170de4a24ae70f4e129
DrizW <129849797+DrizW@users.noreply.github.com>
2024-06-23 17:38:15 +0200

    Initial commit with sensitive information removed

--- server/models/database.js
+++ server/models/database.js
@@ -1,11 +1,11 @@
-const mysql2 = require('mysql2/promise')
+const mysql2 = require("mysql2/promise");
 
 let pool = mysql2.createPool({
-    connectionLimit: 10000,
-    host: "db.3wa.io",// on rentre l'hôte, l'adresse url où se trouve la bdd
-    user: "drisssaikak", // identifiant BDD
-    password: "b12893928871bafe6bf8a571db2c440e", // le password
-    database: "drisssaikak_projet", // nom de la base de donnée
-}) 
+  connectionLimit: 10000,
+  host: "private", // on rentre l'hôte, l'adresse url où se trouve la bdd
+  user: "private", // identifiant BDD
+  password: "private", // le password
+  database: "private", // nom de la base de donnée
+});
 
-module.exports = pool
+module.exports = pool;
